12343f603216a4a143a6e1fed0abebdfef526da8
PNGT-DEV <50997058+PNGT-DEV@users.noreply.github.com>
2021-02-15 19:31:55 +0200

    Create fi.lua
    Translation for esx_drugs

---
+++ locales/fi.lua
@@ -0,0 +1,31 @@
+Locales ['fi'] = {
+   -- weed
+   ['weed_pickupprompt'] = 'paina ~INPUT_CONTEXT~ kerätäksesi ~g~Kannabista~s~.',
+   ['weed_quitprocess'] = 'paina ~INPUT_CONTEXT~ lopettaaksesi ~g~Kannabiksen~s~ keräämistä.',
+   ['weed_inventoryfull'] = 'sinulla ei ole repussa tilaa ~g~Kannabikselle~s~.',
+   ['weed_processprompt'] = 'paina ~INPUT_CONTEXT~ pakataksesi ~g~Kannabista~s~.',
+   ['weed_processingstarted'] = 'muunnetaan ~g~Kannabista~s~ ~g~Marijuanaksi~s~...',
+   ['weed_processingfull'] = 'muuntaminen ~r~lopetettu~s~ reppusi on täynnä!',
+   ['weed_processingenough'] = 'sinulla täytyy olla ~b~3x~s~ ~g~Kannabista~s~ jatkaaksesi.',
+   ['weed_processed'] = 'sinä muunsit ~b~3x~s~ ~g~Kannabista~s~ to ~b~1x~s~ ~g~Marijuanaksi~s~',
+   ['weed_processingtoofar'] = 'muuntaminen ~r~lopetettu~s~ poistuit alueelta.',
+  
+   -- drug dealer
+   ['dealer_prompt'] = 'paina ~INPUT_CONTEXT~ puhuaksesi ~r~Huume Diilaajan~s~ kanssa.',
+   ['dealer_title'] = 'huumeitten välittäjä',
+   ['dealer_item'] = '$%s',
+   ['dealer_notenough'] = 'sinulla ei ole tarpeaksi huumeita myydäksesi!',
+   ['dealer_sold'] = 'sinä myit ~b~%sx~s~ ~y~%s~s~ hintaan ~g~$%s~s~',
+  
+   -- license
+   ['license_title'] = 'sinulta puuttuu huumeitten käsittely lupa, osta se tästä.',
+   ['license_no'] = 'ei',
+   ['license_bought'] = 'sinä ostit ~b~%s~s~ hintaan ~r~$%s~s~',
+   ['license_bought_fail'] = 'sinä et voi ostaa ~b~%s~s~!',
+   ['license_weed'] = 'huumelupa',
+  
+  -- blips
+  ['blip_weedfield'] = 'kannabis farmi',
+  ['blip_weedprocessing'] = 'kannabiksen pakkaus',
+  ['blip_drugdealer'] = 'huumeitten välittäjä',
+}
